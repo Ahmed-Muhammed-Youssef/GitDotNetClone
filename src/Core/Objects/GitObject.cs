19ef4548c8d02a2fe53a369dda3c82beb518b11a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-24 18:52:06 +0300

    feat: add initial commit

---
+++ src/Core/Objects/GitObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.Objects
+{
+    public abstract class GitObject
+    {
+        public abstract string Type { get; }
+        public abstract byte[] Content { get; }
+        public byte[] Serialize()
+        {
+            var header = $"{Type} {Content.Length}\0";
+            var headerBytes = Encoding.UTF8.GetBytes(header);
+            return headerBytes.Concat(Content).ToArray();
+        }
+
+        public string GetHash()
+        {
+            var serialized = Serialize();
+            return Convert.ToHexStringLower(SHA256.HashData(serialized));
+        }
+    }
+}
