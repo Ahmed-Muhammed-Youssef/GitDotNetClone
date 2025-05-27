69b329aca77202ec5796a553997835aa95e58161
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-27 16:24:27 +0300

    feat: add blob git type

---
+++ src/Core/Objects/BlobGitObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Objects
+{
+    public class BlobGitObject(byte[] content) : GitObject
+    {
+        public override string Type => "blob";
+
+        public override byte[] Content { get; } = content;
+    }
+}
