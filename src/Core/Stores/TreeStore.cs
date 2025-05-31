0aec7a8a4c98ea0e602b18241b76a8aa5d4479f5
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-31 17:42:59 +0300

    feat: add empty tree store

---
+++ src/Core/Stores/TreeStore.cs
@@ -0,0 +1,34 @@
+﻿using Core.Index;
+using Core.Objects;
+using Core.Stores.Interfaces;
+
+namespace Core.Stores
+{
+    public class TreeStore(IIndexStore indexStore, string repositoryRoot)
+    {
+        private readonly string _objectsDirectory = Path.Combine(repositoryRoot, ".git", "objects");
+
+        public string BuildTreeFromIndex()
+        {
+
+            indexStore.Load();
+
+            IReadOnlyList<IndexEntry> indexEnteries = indexStore.GetEntries();
+
+            List<TreeEntry> root = BuildTreeRecursive(indexEnteries, "");
+
+            throw new NotImplementedException();
+        }
+
+        private List<TreeEntry> BuildTreeRecursive(IEnumerable<IndexEntry> entries, string currentPath)
+        {
+            throw new NotImplementedException();
+        }
+        private void SaveGitObject(GitObject obj)
+        {
+            var hash = obj.GetHash();
+            var path = Path.Combine(_objectsDirectory, hash);
+            File.WriteAllBytes(path, obj.Serialize());
+        }
+    }
+}
