2eea3fffa4fe2a045be1230c39e7bd68933c4754
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-06-09 10:19:01 +0300

    feat: add a new loading method with deserialization

--- src/Core/Stores/ObjectStore.cs
+++ src/Core/Stores/ObjectStore.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Objects;
+using System.Text.Json;
 
 namespace Core.Stores
 {
@@ -12,6 +13,7 @@ namespace Core.Stores
         /// Loads the byte array of a Git object from the object store using its hash.
         /// </summary>
         /// <param name="hash">The SHA-256 hash of the Git object.</param>
+        /// <param name="rootPath">The root path of the repository (where the <c>.git</c> directory resides).</param>
         /// <returns>The deserialized <see cref="byte[]"/> instance.</returns>
         /// <exception cref="FileNotFoundException">Thrown if the object file does not exist.</exception>
         /// <exception cref="DirectoryNotFoundException">Thrown if the directory is found.</exception>
@@ -26,11 +28,34 @@ namespace Core.Stores
         }
 
         /// <summary>
+        /// Loads a Git object of type <typeparamref name="T"/> from the object store using its hash.
+        /// </summary>
+        /// <typeparam name="T">The type of the Git object to load. Must derive from <see cref="GitObject"/>.</typeparam>
+        /// <param name="hash">The SHA-256 hash of the Git object.</param>
+        /// <param name="rootPath">The root path of the repository (where the <c>.git</c> directory resides).</param>
+        /// <param name="options">The JSON serializer options used to deserialize the object.</param>
+        /// <returns>
+        /// The deserialized Git object of type <typeparamref name="T"/> if found; otherwise, <c>null</c> if deserialization fails.
+        /// </returns>
+        /// <exception cref="FileNotFoundException">Thrown if the object file does not exist.</exception>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the expected object directory is missing.</exception>
+        /// <exception cref="JsonException">Thrown if the object content cannot be deserialized into the specified type.</exception>
+        public static T? Load<T>(string hash, string rootPath, JsonSerializerOptions options) where T : GitObject
+        {
+            string filePath = Path.Combine(rootPath, ".git", "objects", hash[..2], hash[2..]);
+
+            byte[] data = File.ReadAllBytes(filePath);
+
+            return JsonSerializer.Deserialize<T>(data, options);
+        }
+
+        /// <summary>
         /// Saves a Git object to the object store if it does not already exist.
         /// Git objects are stored under .git/objects/xx/yyyy... where "xx" is the first
         /// two characters of the object's hash and "yyyy..." is the remaining part.
         /// </summary>
         /// <param name="obj">The <see cref="GitObject"/> to save.</param>
+        /// <param name="rootPath">The root path of the repository (where the <c>.git</c> directory resides).</param>
         /// <exception cref="InvalidOperationException">
         /// Thrown if a hash collision occurs (i.e., an object with the same hash exists but with different content).
         /// </exception>
